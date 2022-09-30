41addcffa67056a97948f9c547e034baac857e44
Youva GAUDE <ygaude@student.42.fr>
2022-09-30 19:31:26 +0200

    adapted code from https://youtu.be/WnUzWuaMzuM

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,95 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::ScalingMode, render::texture::ImageSettings};
+
+pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+pub const RESOLUTION: f32 = 16.0 / 9.0;
 
 fn main() {
-    App::new().run();
+    let height = 900.0;
+    App::new()
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(WindowDescriptor {
+            width: height * RESOLUTION,
+            height,
+            title: "Bevy".to_string(),
+            present_mode: bevy::window::PresentMode::Fifo,
+            resizable: false,
+            ..Default::default()
+        })
+        .insert_resource(ImageSettings::default_nearest())
+        .add_startup_system_to_stage(StartupStage::PreStartup, load_ascii)
+        .add_startup_system(spawn_camera)
+        .add_startup_system(spawn_player)
+        .add_plugins(DefaultPlugins)
+        .run();
+}
+
+fn spawn_player(mut commands: Commands, ascii: Res<AsciiSheet>) {
+    let mut sprite = TextureAtlasSprite::new(1);
+    sprite.color = Color::rgb(0.3, 0.3, 0.9);
+    sprite.custom_size = Some(Vec2::splat(1.0));
+    let player = commands
+        .spawn_bundle(SpriteSheetBundle {
+            sprite,
+            texture_atlas: ascii.0.clone(),
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 900.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(Name::new("Player"))
+        .id(); // id() gives back the entity after creation
+
+    let mut background_sprite = TextureAtlasSprite::new(0);
+    background_sprite.color = Color::rgb(0.5, 0.5, 0.5);
+    background_sprite.custom_size = Some(Vec2::splat(1.0));
+    let background = commands
+        .spawn_bundle(SpriteSheetBundle {
+            sprite: background_sprite,
+            texture_atlas: ascii.0.clone(),
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, -1.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(Name::new("Background"))
+        .id(); // id() gives back the entity after creation
+
+    commands.entity(player).push_children(&[background]);
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn_bundle(Camera2dBundle {
+        projection: OrthographicProjection {
+            left: -1.0 * RESOLUTION,
+            right: 1.0 * RESOLUTION,
+            bottom: -1.0,
+            top: 1.0,
+            scaling_mode: ScalingMode::None,
+            scale: 1.0,
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+}
+
+struct AsciiSheet(Handle<TextureAtlas>);
+
+fn load_ascii(
+    mut commands: Commands,
+    assets: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let image = assets.load("Ascii.png");
+    let atlas = TextureAtlas::from_grid_with_padding(
+        image,
+        Vec2::splat(9.0),
+        16,
+        16,
+        Vec2::splat(2.0),
+        Vec2::splat(0.0),
+    );
+    let atlas_handle = texture_atlases.add(atlas);
+    commands.insert_resource(AsciiSheet(atlas_handle));
 }
